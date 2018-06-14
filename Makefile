334b5b5683c02fdce56dc58d7bc126b0f350e4ba
jamie young <jmeyoung@gmail.com>
2018-06-14 18:31:23 -0400

    devops: cronjob and secret work
    Squashed commit of the following:
    
    commit 5677240676a8119b25ae5195ed45caf14c365815
    Author: jamie young <jmeyoung@gmail.com>
    Date:   Thu Jun 14 18:29:27 2018 -0400
    
        .
    
    commit c75cb705d4ee1e5a54b07700696859c0762cee97
    Author: jamie young <jmeyoung@gmail.com>
    Date:   Thu Jun 14 13:01:21 2018 -0400
    
        .

--- Makefile
+++ Makefile
@@ -4,15 +4,41 @@ project_name = npm-token-rotation
 version = 0.0.1
 docker_tag = ${repository_name}/${project_name}:${version}-${build_id}
 
+
+.PHONY: build run push clean
+
+
 build:
 	docker build --tag ${docker_tag} .
 
-run: build
-	docker run --rm -e "NPM_TOKEN=${NPM_TOKEN}" -it ${docker_tag} /bin/bash -- jobs/rotate-npm-token.sh
+
+run: check-env build
+	docker run --rm \
+	-e NPM_TOKEN=${NPM_TOKEN} \
+	-e NPM_USER=${NPM_USER} \
+	-e NPM_PASS=${NPM_PASS} \
+	-it ${docker_tag} /bin/bash -- jobs/rotate-npm-token.sh 
+
 
 push: build
 	docker push ${docker_tag}
 
+
+check-env:
+ifeq ($(NPM_TOKEN),)
+	$(error NPM_TOKEN is undefined)
+endif
+ifeq ($(NPM_USER),)
+	$(error NPM_USER is undefined)
+endif
+ifeq ($(NPM_PASS),)
+	$(error NPM_PASS is undefined)
+endif
+
+
 clean:
-	docker ps -a | grep "${repository_name}/${project_name}" | awk '{print $$1}' | xargs docker rm
-	docker images -a ${repository_name}/${project_name} | grep -v REPO | awk '{print $$3}' | xargs docker rmi -f
+	docker ps -a | grep "${repository_name}/${project_name}" | \
+	awk '{print $$1}' | xargs docker rm
+
+	docker images -a ${repository_name}/${project_name} | grep -v REPO | \
+	awk '{print $$3}' | xargs docker rmi -f
--- jobs/rotate-npm-token.sh
+++ jobs/rotate-npm-token.sh
@@ -7,4 +7,67 @@ if [ -z ${NPM_TOKEN} ]; then
   exit 1
 fi
 
-npm token list
+if [ -z ${NPM_USER} ]; then
+  echo "Environment variable NPM_USER unset; value: ${NPM_USER:-undefined}"
+  exit 1
+fi
+
+if [ -z ${NPM_PASS} ]; then
+  echo "Environment variable NPM_PASS unset; value: ${NPM_PASS:-undefined}"
+  exit 1
+fi
+
+# Purpose
+# - Get the readonly token ids for the autenticated user being doubly careful not to include the one
+#   that has been used to authenticate with to run this process. Yes, we know it wouldn't be
+#   readonly; We are checking anyway.
+#
+# - Revoke all the ids returned
+#
+# - Create a new readonly token
+#
+# - Store the readonly token somewhere
+
+
+
+# GET ALL READONLY TOKEN IDS
+#
+#  $ npm token ls
+# ┌────────┬─────────┬────────────┬──────────┬────────────────┐
+# │ id     │ token   │ created    │ readonly │ CIDR whitelist │
+# ├────────┼─────────┼────────────┼──────────┼────────────────┤
+# │ e025b1 │ 2b0237… │ 2018-06-14 │ yes      │                │
+# ├────────┼─────────┼────────────┼──────────┼────────────────┤
+# │ 76a4ed │ fc1a0d… │ 2015-12-03 │ no       │                │
+# └────────┴─────────┴────────────┴──────────┴────────────────┘
+# ^ ^^^^^^ ^ ^^^^^^^ ^ ^^^^^^^^^^ ^ ^^^
+# $1  $2  $3   $4   $5     $6    $7 $8
+#
+# IF
+#   token ($4) does not match the first 6 characters of $NPM_TOKEN
+# AND
+#   readonly ($8) is `yes`
+# THEN
+#   return the id ($2)      _-(cheating with grep; wtfe)
+#                          /
+# $ npm token list | grep yes | awk -v pat="${NPM_TOKEN:0:6}" '$4 !~ pat {print $2}'
+# e025b1
+
+read_only_ids=(`npm token list | grep yes | awk -v pat="${NPM_TOKEN:0:6}" '$4 !~ pat {print $2}'`)
+
+
+# printf "%s\n" "${read_only_ids[@]}"
+if [[ ( ${#read_only_ids[@]} > 0 ) ]]; then
+  echo "Revoking ${read_only_ids[@]}"
+  npm token revoke ${read_only_ids[@]}
+  echo "Done!"
+else
+  echo "No tokens to revert"
+fi
+
+echo "Creating read-only token"
+read_only_token=`echo "${NPM_PASS}" | npm token create --read-only | grep token | awk '{print $4}'`
+echo "Done!"
+
+echo "${read_only_token}"
+
